7f16cd937f1a14ee119b4eb9d70196f06a04bd3e
calandile <calandile@gmail.com>
2022-05-28 18:14:32 +0200

    getCart() implementation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\Cart;
-use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -12,7 +11,7 @@ class ProductController extends Controller
     public function getIndex()
     {
         $products = Product::all();
-        return view('order.main', ['products' => $products]);
+        return view('order.main')->with(compact('products'));
     }
 
     public function addToCart(Request $request, $id)
@@ -23,7 +22,19 @@ class ProductController extends Controller
         $cart->add($product, $product->id);
 
         $request->session()->put('cart', $cart);
-        dd($request->session()->get('cart'));
-        return redirect()->route('order.index');
+        // dd($request->session()->get('cart'));
+        return redirect()->route('order.main');
+    }
+
+    public function getCart(Request $request)
+    {
+        if (!$request->session()->has('cart')) {
+            return view('order.pages.shopping-cart');
+        }
+
+        $oldCart = $request->session()->get('cart');
+        $cart = new Cart($oldCart);
+
+        return view('order.pages.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
     }
 }
