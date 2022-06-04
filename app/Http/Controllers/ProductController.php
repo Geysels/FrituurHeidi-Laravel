2774fda8dc5b5cbd876c6a2ca3d5bd340d5f3cf6
calandile <calandile@gmail.com>
2022-06-04 15:06:02 +0200

    Moved index function to the ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,39 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
-use App\Cart;
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    public function getIndex()
+    public function index()
     {
         $products = Product::all();
         return view('order.main')->with(compact('products'));
-    }
-
-    public function addToCart(Request $request, $id)
-    {
-        $product = Product::find($id);
-        $oldCart = $request->session()->has('cart') ? $request->session()->get('cart') : null;
-        $cart = new Cart($oldCart);
-        $cart->add($product, $product->id);
-
-        $request->session()->put('cart', $cart);
-        // dd($request->session()->get('cart'));
-        return redirect()->route('order.main');
-    }
-
-    public function getCart(Request $request)
-    {
-        if (!$request->session()->has('cart')) {
-            return view('order.pages.shopping-cart');
-        }
-
-        $oldCart = $request->session()->get('cart');
-        $cart = new Cart($oldCart);
-
-        return view('order.pages.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
     }
 }
