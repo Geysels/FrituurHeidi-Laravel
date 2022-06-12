aef1c837e21f40429acdef0c827617f021d4958f
calandile <calandile@gmail.com>
2022-06-12 19:55:51 +0200

    Layout changes

--- resources/views/order/layouts/default.blade.php
+++ resources/views/order/layouts/default.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="cupcake">
+<html lang="en" data-theme="bumblebee">
 
 <head>
     @include('includes.head')
 </head>
 
-<body>
-    <div class="drawer drawer-mobile">
+<body class="scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100 bg-[url('/img/wave.svg')] bg-no-repeat">
+    <div class="drawer drawer-mobile justify-center">
         <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col">
+        <div
+            class="drawer-content scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100 flex flex-col bg-stone-50">
             @yield('content')
         </div>
         @include('includes.sidebar')
--- resources/views/order/pages/contents/menu-content.blade.php
+++ resources/views/order/pages/contents/menu-content.blade.php
@@ -1,13 +1,14 @@
 @foreach ($products as $product)
-    <div class="flex justify-between">
-        <p>{{ $product->name }}</p>
-        <img src="{{ asset('img/dummy.png') }}" alt="">
-        <div class="btn-group">
-            <button class="btn btn-active"> {{ $product->price }}
-            </button>
-            <a href="{{ route('addToCart', ['id' => $product->id]) }}"><button class="btn"> <i
-                        class="fa-solid fa-plus"></i></a>
+    <div class="flex justify-between p-4 md:p-10">
+        <div>
+            <p class="text-xl font-semibold">{{ $product->name }}</p>
+            <p class="italic">{{ $product->remark }}</p>
+            <p class="text-xl font-semibold">€ {{ $product->price }}</p>
+            <a href="{{ route('addToCart', ['id' => $product->id]) }}"><button
+                    class="btn btn-secondary mt-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">Toevoegen</a>
             </button>
         </div>
+        <img src="{{ asset('img/dummy.png') }}" alt="">
     </div>
+    <div class="divider"></div>
 @endforeach
