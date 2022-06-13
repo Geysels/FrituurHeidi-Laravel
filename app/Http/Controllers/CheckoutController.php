c0f8db09897d73e1ac4326b93d78e129fb12dd05
calandile <calandile@gmail.com>
2022-06-13 14:27:20 +0200

    Added submitOrder()

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Cart;
 use App\Category;
+use App\Order;
 
 class CheckoutController extends Controller
 {
@@ -18,5 +19,16 @@ class CheckoutController extends Controller
             'totalPrice' => $cart->getTotalPrice(),
             'categories' => $categories,
         ]);
+    }
+
+    public function submitOrder()
+    {
+        $order = new Order;
+
+        $order->name = request('name');
+        $order->email = request('email');
+        $order->telephone = request('telephone');
+        dd($order);
+        // $order->save();
     }
 }
