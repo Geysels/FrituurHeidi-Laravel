a246346d538b9738b5b0bb7a9473003f6eaa7985
calandile <calandile@gmail.com>
2022-06-20 14:27:05 +0200

    Fetching categories with products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,14 +7,33 @@ use App\Cart;
 use App\Option;
 use Illuminate\Http\Request;
 use App\Product;
+use App\OptionProduct;
 
 class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::all();
-        $categories = Category::all();
-        $options = Option::all();
+        $categories = Category::where('visible', '=', '1')->get();
+
+        foreach ($categories as $category) {
+            $products = Product::where('category_id', '=', $category->id)->get();
+            foreach ($products as $product) {
+                $prod = Product::find($product->id);
+                $productOptions = OptionProduct::where('product_id', '=', $product->id)->get();
+                foreach ($productOptions as $option) {
+                    $opt = Option::find($option->option_id);
+                    $option->name = $opt->name;
+                    $option->price = $opt->price;
+                }
+                $product->options = $productOptions;
+                $product->id = $prod->id;
+                $product->name = $prod->name;
+                $product->visible = $prod->visible;
+                $product->price = $prod->price;
+                $product->remark = $prod->remark;
+            }
+            $category->products = $products;
+        }
 
         $cart = Cart::getInstance($request);
 
@@ -23,7 +42,6 @@ class ProductController extends Controller
             'totalPrice' => $cart->getTotalPrice(),
             'products' => $products,
             'categories' => $categories,
-            'options' => $options,
         ]);
     }
 }
