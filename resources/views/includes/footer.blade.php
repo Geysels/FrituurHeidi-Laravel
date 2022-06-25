f3ba820a69d56a541c5cb3c32a987620d199bc97
calandile <calandile@gmail.com>
2022-06-25 18:28:45 +0200

    Added opening hours

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,28 +1,65 @@
-<footer class="footer bg-base-200 text-base-content p-10" id="contactus">
-    <div class="flex flex-col">
-        <span class="footer-title">Contactgegevens</span>
-        <div class="flex">
-            <span class="mr-3 justify-center"><i class="fa-solid fa-location-dot fa-lg"></i></span>
-            <p class="text-base text-slate-50">Kazernelaan 124,
-                3530
-                Houthalen-Helchteren</p>
-        </div>
-        <div class="flex">
-            <span class="mr-3 justify-center"><i class="fa-solid fa-phone fa-lg"></i></span>
-            <p class="text-base text-slate-50">0456/01 52 97</p>
-        </div>
-        <div class="flex">
-            <span class="mr-3 justify-center"><i class="fa-solid fa-envelope fa-lg"></i></span>
-            <p class="text-base text-slate-50">info@frituurheidi.com</p>
-        </div>
-    </div>
-    <div>
+<footer class="footer bg-base-200 text-base-content p-10 text-lg" id="contactus">
+    <div class="w-96">
         <div class="flex flex-col">
             <span class="footer-title">Openingsuren</span>
             <p class="italic">Onderstaande openingsuren zijn in normale omstandigheden voor meer informatie over de
                 openingsuren tijdens
                 feestdagen of bij ziekte worden geplaatst op onze facebook.</p>
         </div>
+        <div class="flex justify-between space-x-4">
+            <div class="flex flex-col text-slate-50">
+                <p>Maandag</p>
+                <p>Dinsdag</p>
+                <p>Woensdag</p>
+                <p>Donderdag</p>
+                <p>Vrijdag</p>
+                <p>Zaterdag</p>
+                <p>Zondag</p>
+            </div>
+            <div class="flex flex-col">
+                <p>GESLOTEN</p>
+                <p>GESLOTEN</p>
+                <p>17:00</p>
+                <p>17:00</p>
+                <p>17:00</p>
+                <p>17:00</p>
+                <p>17:00</p>
+            </div>
+            <div class="flex flex-col">
+                <p>GESLOTEN</p>
+                <p>GESLOTEN</p>
+                <p>20:30</p>
+                <p>20:30</p>
+                <p>20:30</p>
+                <p>20:30</p>
+                <p>20:30</p>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col">
+        <span class="footer-title">Contactgegevens</span>
+        <div class="flex">
+            <span class="mr-3 justify-center"><i class="fa-solid fa-phone fa-lg"></i></span>
+            <a href="tel:0456015297">
+                <p class="text-base text-slate-50">0456/01 52 97</p>
+            </a>
+        </div>
+        <div class="flex">
+            <span class="mr-3 justify-center"><i class="fa-solid fa-envelope fa-lg"></i></span>
+            <a href="mailto:info@frituurheidi.com">
+                <p class="text-base text-slate-50">info@frituurheidi.com</p>
+            </a>
+        </div>
+        <div class="flex lg:hidden">
+            <span class="mr-3 justify-center"><i class="fa-solid fa-location-dot fa-lg"></i></span>
+            <a href="https://goo.gl/maps/ngJq71TSt642Rko79">
+                <p class="text-base text-slate-50">Kazernelaan 124, 3530 Houthalen-Helchteren</p>
+            </a>
+        </div>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2507.605768293609!2d5.395229115753556!3d51.06036677956415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c12884abbfe6ad%3A0xb9329e7189686060!2sFrituur%20Heidi!5e0!3m2!1sen!2sbe!4v1656173514239!5m2!1sen!2sbe"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="hidden lg:flex"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </footer>
 <footer class="footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
