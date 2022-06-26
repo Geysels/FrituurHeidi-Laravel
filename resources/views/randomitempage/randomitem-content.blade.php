1b58c9f543d037dbc24660049a09d2b78e1675af
miguelB17 <miguelstitch@gmail.com>
2022-06-27 01:32:54 +0200

    random page layout

--- resources/views/randomitempage/randomitem-content.blade.php
+++ resources/views/randomitempage/randomitem-content.blade.php
@@ -1,6 +1,11 @@
 @extends('public.layouts.default')
 @section('content')
-    <section>
-        
+<section>
+        <div class="flex min-h-screen justify-center bg-cover bg-center md:justify-start"
+            style="background-image: url({{ asset('img/bestellen4.jpeg') }})">
+            <div class="flex w-screen flex-col justify-center bg-stone-900 bg-opacity-80 p-6 text-center xl:p-48">
+                <h1 class="mb-7 text-center text-8xl">Keuzestress?</h1>
+               
+        </div>
     </section>
 @endsection
